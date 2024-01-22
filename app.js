7e3c8ac980689b6fedee8b70760e84283587657d
asku1990 <a-p.kvs@pm.me>
2024-01-22 21:38:54 +0200

    you can get contactinfo by language (fi or en)

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const contactInfoRoutes = require('./routes/contactInfoRoutes');
 const db = require('./db');
+const cors = require('cors'); // Import the cors middleware
+
+
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+// Use the cors middleware
+app.use(cors());
 
 // Use the contactInfoRoutes for the '/contact-info' endpoint
 app.use('/contactinfo', contactInfoRoutes);
--- routes/contactInfoRoutes.js
+++ routes/contactInfoRoutes.js
@@ -1,16 +1,36 @@
 // contactInfoRoutes.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../db'); // Adjust the path accordingly
+const db = require("../db"); // Adjust the path accordingly
 
 // Route handling
-router.get('/', (req, res) => {
+
+//get all http://localhost:3000/contactinfo
+router.get("/", (req, res) => {
   // Sample query to retrieve data from a table
-  db.query('SELECT * FROM contactInfo', (err, results) => {
+  db.query("SELECT * FROM contactInfo", (err, results) => {
     if (err) {
-      console.error('Error executing MySQL query:', err);
-      res.status(500).send('Internal Server Error');
+      console.error("Error executing MySQL query:", err);
+      res.status(500).send("Internal Server Error");
     } else {
+      res.json(results);
+    }
+  });
+});
+
+router.get("/lang", (req, res) => {
+  const lang = req.headers["accept-language"]; // Extract language from header
+  //console.log("lang",lang)
+
+  // Modify your database query based on the language, if necessary
+  const query = "SELECT * FROM contactinfo WHERE language = ?"; // Adjust the query based on 'lang'
+
+  db.query(query, [lang], (err, results) => {
+    if (err) {
+      console.error("Error executing MySQL query:", err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      // Optionally, process results based on the language
       res.json(results);
     }
   });
