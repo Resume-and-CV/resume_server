901b717fb456b3ab445a802d492852d0180a8b05
asku1990 <a-p.kvs@pm.me>
2024-01-29 19:20:22 +0200

    JAWSDB / local db connection ;

--- db.js
+++ db.js
@@ -1,5 +1,48 @@
 // db.js
+
 const mysql = require("mysql");
+require("dotenv").config();
+const url = require('url');
+
+// Parse the JAWSDB_URL environment variable
+let dbConfig;
+if (process.env.JAWSDB_URL) {
+  // If running on Heroku with JawsDB
+  const jawsDbUrl = url.parse(process.env.JAWSDB_URL);
+  dbConfig = {
+    host: jawsDbUrl.hostname,
+    user: jawsDbUrl.auth.split(':')[0],
+    password: jawsDbUrl.auth.split(':')[1],
+    database: jawsDbUrl.pathname.slice(1)
+  };
+} else {
+  // Fallback for local development
+  dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+  };
+}
+
+const db = mysql.createConnection(dbConfig);
+
+// Connect to MySQL
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    throw err;
+  }
+  console.log("Connected to MySQL server");
+});
+
+module.exports = db;
+
+
+
+
+
+/* const mysql = require("mysql");
 require("dotenv").config();
 
 const dbConfig = {
@@ -19,15 +62,5 @@ db.connect((err) => {
   }
   console.log("Connected to MySQL server");
 });
-module.exports = db;
+module.exports = db; */
 
-/* const { Pool } = require('pg')
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-})
-
-module.exports = pool;  */
