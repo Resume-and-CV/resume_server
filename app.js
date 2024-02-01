c87687e7fea535dc1deb02eaa56f887443fca30c
asku1990 <a-p.kvs@pm.me>
2024-02-01 19:36:50 +0200

    Added hobby and language skills

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const loginRoutes = require("./routes/loginRoutes");
 const educationRoutes = require("./routes/educationRoutes");
 const workRoutes = require("./routes/workRoutes");
 const userRoutes = require("./routes/userRoutes");
+const languageInfo = require("./routes/languageInfo");
+const hobbiesRoutes = require("./routes/hobbiesInfo")
 
 require("dotenv").config(); // Load environment variables from .env file
 
@@ -21,6 +23,8 @@ app.use("/login", loginRoutes);
 app.use("/education", educationRoutes);
 app.use("/work", workRoutes);
 app.use("/user", userRoutes);
+app.use("/languageinfo", languageInfo);
+app.use("/hobbiesinfo", hobbiesRoutes);
 
 const port = process.env.PORT || 3000; // Define the server port
 
