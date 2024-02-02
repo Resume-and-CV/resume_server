b3a6a2151ca2dc476fb76600c100b90d42e4e55c
asku1990 <a-p.kvs@pm.me>
2024-02-02 19:52:47 +0200

    now you can see my school and personal projects

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const loginRoutes = require("./routes/loginRoutes");
 const educationRoutes = require("./routes/educationRoutes");
 const workRoutes = require("./routes/workRoutes");
 const userRoutes = require("./routes/userRoutes");
-const languageInfo = require("./routes/languageInfo");
+const languageInfoRoutes = require("./routes/languageInfo");
 const hobbiesRoutes = require("./routes/hobbiesInfo")
 const recommendationRoutes = require("./routes/recommendationRoutes.js")
+const schoolProjectRoutes = require("./routes/schoolProjectRoutes.js")
+const personalProjectRoutes = require("./routes/personalProjectRoutes.js")
 
 require("dotenv").config(); // Load environment variables from .env file
 
@@ -24,9 +26,11 @@ app.use("/login", loginRoutes);
 app.use("/education", educationRoutes);
 app.use("/work", workRoutes);
 app.use("/user", userRoutes);
-app.use("/languageinfo", languageInfo);
+app.use("/languageinfo", languageInfoRoutes);
 app.use("/hobbiesinfo", hobbiesRoutes);
 app.use("/recommendations", recommendationRoutes);
+app.use("/schoolprojects", schoolProjectRoutes);
+app.use("/personalprojects", personalInfoRoutes)
 
 const port = process.env.PORT || 3000; // Define the server port
 
