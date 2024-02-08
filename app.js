9ec6987f7d3c4d734a743353f45ed6a58400e577
asku1990 <a-p.kvs@pm.me>
2024-02-08 07:38:04 +0200

    mailtrap service send testmails

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const db = require("./db"); // Import database configuration
+const bodyParser = require("body-parser");
+
 const contactInfoRoutes = require("./routes/contactInfoRoutes");
 const personalInfoRoutes = require("./routes/personalInfoRoutes");
 const loginRoutes = require("./routes/loginRoutes");
@@ -12,6 +14,7 @@ const hobbiesRoutes = require("./routes/hobbiesInfo")
 const recommendationRoutes = require("./routes/recommendationRoutes.js")
 const schoolProjectRoutes = require("./routes/schoolProjectRoutes.js")
 const personalProjectRoutes = require("./routes/personalProjectRoutes.js")
+const mailTrapRoutes = require("./routes/mailTrapRoutes.js")
 
 require("dotenv").config(); // Load environment variables from .env file
 
@@ -20,6 +23,7 @@ app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(cors()); // Enable CORS for all routes
 
 // Register route handlers
+app.use(bodyParser.json());
 app.use("/contactinfo", contactInfoRoutes);
 app.use("/personalinfo", personalInfoRoutes);
 app.use("/login", loginRoutes);
@@ -31,6 +35,7 @@ app.use("/hobbiesinfo", hobbiesRoutes);
 app.use("/recommendations", recommendationRoutes);
 app.use("/schoolprojects", schoolProjectRoutes);
 app.use("/personalprojects", personalProjectRoutes)
+app.use("/email", mailTrapRoutes);
 
 const port = process.env.PORT || 3000; // Define the server port
 
---
+++ routes/mailTrapRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const router = express.Router();
+
+const mailtrapService = require("../middleware/mailTrtapService");
+
+router.post("/send-mailtrap", async (req, res) => {
+  try {
+    const response = await mailtrapService.sendMail(req, res);
+    res.status(200).json({
+      status: "success",
+      message: "Email sent successfully",
+      data: response,
+    });
+  } catch (error) {
+    console.log("error", error);
+    res.status(400).json({
+      status: "error",
+      message: "Email not sent",
+    });
+  }
+});
+
+module.exports = router;
