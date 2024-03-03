2d5307d8ac56a60a36cd1ae917584c08ba15c290
asku1990 <a-p.kvs@pm.me>
2024-03-03 13:08:19 +0200

    all education routes are working. add, get and delete

--- routes/educationRoutes/examption.js
+++ routes/educationRoutes/examption.js
@@ -5,18 +5,52 @@ const authenticateToken = require('../../middleware/authenticateToken') // Impor
 
 //get exemptions by course_id
 const getExemptionsByCourseId = async (req, res) => {
-  const course_id = req.query.course_id // Extract course_id from URL parameters
+  const course_id = req.headers['course_id']
+  const lang = req.headers['accept-language'] // Extract language from header
+
+  // Check if education_id and lang are provided
+  if (!course_id) {
+    return res.status(400).json({ message: 'Missing course_id in headers' })
+  }
+  if (!lang) {
+    return res
+      .status(400)
+      .json({ message: 'Missing accept-language in headers' })
+  }
 
   try {
-    // Modify your database query based on the course_id
+    // Modify your database query based on the language, if necessary
     const [results] = await db.query(
-      'SELECT * FROM exemptions WHERE course_id = ?',
-      [course_id],
+      `
+                SELECT 
+                e.exemption_id,
+                e.course_id,
+                et.original_institution,
+                et.original_course_name,
+                e.original_credits, 
+                e.completion_date, 
+                e.type,
+                et.language
+            FROM 
+                exemptions_new e 
+            JOIN 
+                exemptions_translations_new et 
+            ON 
+                e.exemption_id = et.exemption_id
+            WHERE
+                et.language = ? AND
+                e.course_id = ?
+            ORDER BY 
+                e.completion_date ASC
+            `,
+      [lang, course_id],
     )
 
     // Check if results is empty
     if (results.length === 0) {
-      return res.status(200).json([]) // Return an empty array with a 200 status code
+      return res.status(404).json({
+        message: 'No exemptions found for the provided language and course_id',
+      })
     }
 
     // Process and respond with results based on the language
@@ -29,84 +63,43 @@ const getExemptionsByCourseId = async (req, res) => {
   }
 }
 
-// Post a new exemption
 const addExemptionToCourseId = async (req, res) => {
   const {
     course_id,
-    original_institution,
-    original_course_name,
     original_credits,
     completion_date,
     type,
-    language,
+    translations, // This should be an array of objects
   } = req.body
 
   try {
-    const [results] = await db.query(
-      'INSERT INTO exemptions (course_id, original_institution, original_course_name, original_credits, completion_date, type, language) VALUES (?, ?, ?, ?, ?, ?, ?)',
-      [
-        course_id,
-        original_institution,
-        original_course_name,
-        original_credits,
-        completion_date,
-        type,
-        language,
-      ],
+    // First, insert into the exemptions_new table
+    const [exemptionResults] = await db.query(
+      'INSERT INTO exemptions_new (course_id, original_credits, completion_date, type) VALUES (?, ?, ?, ?)',
+      [course_id, original_credits, completion_date, type],
     )
 
-    // Respond with the id of the newly created exemption
-    res.json({ id: results.insertId })
-  } catch (err) {
-    console.error('Error executing MySQL query:', err)
-    res
-      .status(500)
-      .json({ message: 'Internal Server Error', error: err.message })
-  }
-}
+    // Get the ID of the newly inserted exemption
+    const exemptionId = exemptionResults.insertId
 
-// Update an exemption
-const updateExemptionById = async (req, res) => {
-  const {
-    exemption_id,
-    course_id,
-    original_institution,
-    original_course_name,
-    original_credits,
-    completion_date,
-    type,
-    language,
-  } = req.body
-
-  try {
-    const [results] = await db.query(
-      'UPDATE exemptions SET course_id = ?, original_institution = ?, original_course_name = ?, original_credits = ?, completion_date = ?, type = ?, language = ? WHERE exemption_id = ?',
-      [
-        course_id,
-        original_institution,
-        original_course_name,
-        original_credits,
-        completion_date,
-        type,
-        language,
-        exemption_id,
-      ],
-    )
-
-    // Check if any rows were updated
-    if (results.affectedRows === 0) {
-      return res.status(404).json({
-        message: 'No exemption found with the provided id',
-      })
+    // Then, insert into the exemptions_translations_new table for each translation
+    for (let translation of translations) {
+      const { original_institution, original_course_name, language } =
+        translation
+      await db.query(
+        'INSERT INTO exemptions_translations_new (exemption_id, original_institution, original_course_name, language) VALUES (?, ?, ?, ?)',
+        [exemptionId, original_institution, original_course_name, language],
+      )
     }
 
-    // Respond with a success message
-    res.json({ message: 'Exemption updated successfully' })
+    // Respond with the id of the newly created exemption
+    res.json({ exemptionId, translations })
   } catch (err) {
     console.error('Error executing MySQL query:', err)
-    res
-      .status(500)
-      .json({ message: 'Internal Server Error', error: err.message })
+    res.status(500).json({
+      message: 'An error occurred while adding exemption',
+      error: err.message,
+    })
   }
 }
 
@@ -115,22 +108,45 @@ const deleteExemptionById = async (req, res) => {
   const id = req.params.id
 
   try {
+    // Check if exemption exists
+    const [exemptions] = await db.query(
+      'SELECT * FROM exemptions_new WHERE exemption_id = ?',
+      [id],
+    )
+    if (exemptions.length === 0) {
+      return res
+        .status(404)
+        .json({ message: 'No exemption found with the provided id' })
+    }
+
+    // Delete translations from exemptions_translations_new table
+    await db.query(
+      'DELETE FROM exemptions_translations_new WHERE exemption_id = ?',
+      [id],
+    )
+
+    // Delete exemption from exemptions_new table
     const [result] = await db.query(
-      'DELETE FROM exemptions WHERE exemption_id = ?',
+      'DELETE FROM exemptions_new WHERE exemption_id = ?',
       [id],
     )
 
     // Check if any rows were deleted
     if (result.affectedRows === 0) {
-      return res.status(404).json({
-        message: 'No exemptions found with the provided exemption_id',
-      })
+      return res
+        .status(404)
+        .json({ message: 'No exemption found with the provided id' })
     }
 
     // Respond with a success message
-    res.json({ message: 'Education deleted successfully' })
+    res.json({ message: 'Exemption deleted successfully' })
   } catch (err) {
     console.error('Error executing MySQL query:', err)
+    if (err.code === 'ER_ROW_IS_REFERENCED_2') {
+      return res
+        .status(400)
+        .json({ message: 'You must delete related data first' })
+    }
     res
       .status(500)
       .json({ message: 'Internal Server Error', error: err.message })
@@ -138,9 +154,8 @@ const deleteExemptionById = async (req, res) => {
 }
 
 // Exemption routes
-router.get('/', authenticateToken, getExemptionsByCourseId)
+router.get('/lang', authenticateToken, getExemptionsByCourseId)
 router.post('/add', authenticateToken, addExemptionToCourseId)
-router.put('/update', authenticateToken, updateExemptionById)
 router.delete('/delete/:id', authenticateToken, deleteExemptionById)
 
 module.exports = router
