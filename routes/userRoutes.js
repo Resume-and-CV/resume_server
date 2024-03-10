2f6797195c0b285b1a340d0747167b4aae13b76f
asku1990 <a-p.kvs@pm.me>
2024-03-10 16:06:09 +0200

    useroutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,7 @@ const isAdmin = require('../middleware/isAdmin') // Import the isAdmin middlewar
 
 const saltRounds = 10 // You can adjust this as per your security requirement
 
-router.post('/add', authenticateToken, async (req, res) => {
+router.post('/add', authenticateToken, isAdmin, async (req, res) => {
   const { username, password, type } = req.body
 
   try {
@@ -63,56 +63,61 @@ router.get('/', authenticateToken, isAdmin, async (req, res) => {
 })
 
 // DELETE route to remove a user
-router.delete('/delete/:username', authenticateToken, async (req, res) => {
-  const username = req.params.username
-  let connection // Declare connection outside to ensure it's accessible in both try and catch blocks
+router.delete(
+  '/delete/:username',
+  authenticateToken,
+  isAdmin,
+  async (req, res) => {
+    const username = req.params.username
+    let connection // Declare connection outside to ensure it's accessible in both try and catch blocks
 
-  try {
-    connection = await db.getConnection() // Get a connection from the pool
-    await connection.beginTransaction() // Start a transaction
+    try {
+      connection = await db.getConnection() // Get a connection from the pool
+      await connection.beginTransaction() // Start a transaction
 
-    // Delete UserSessions first
-    await connection.query(
-      'DELETE FROM UserSessions WHERE user_id = (SELECT user_id FROM users WHERE username = ?)',
-      [username],
-    )
-    // Then delete ExpiringLinks
-    await connection.query(
-      'DELETE FROM ExpiringLinks WHERE user_id = (SELECT user_id FROM users WHERE username = ?)',
-      [username],
-    )
+      // Delete UserSessions first
+      await connection.query(
+        'DELETE FROM UserSessions WHERE user_id = (SELECT user_id FROM users WHERE username = ?)',
+        [username],
+      )
+      // Then delete ExpiringLinks
+      await connection.query(
+        'DELETE FROM ExpiringLinks WHERE user_id = (SELECT user_id FROM users WHERE username = ?)',
+        [username],
+      )
 
-    // Delete headerTexts
-    await connection.query(
-      'DELETE FROM headerText WHERE user_id = (SELECT user_id FROM users WHERE username = ?)',
-      [username],
-    )
-    // last delete user
-    const [userDeleteResult] = await connection.query(
-      'DELETE FROM users WHERE username = ?',
-      [username],
-    )
+      // Delete headerTexts
+      await connection.query(
+        'DELETE FROM headerText WHERE user_id = (SELECT user_id FROM users WHERE username = ?)',
+        [username],
+      )
+      // last delete user
+      const [userDeleteResult] = await connection.query(
+        'DELETE FROM users WHERE username = ?',
+        [username],
+      )
 
-    if (userDeleteResult.affectedRows === 0) {
-      await connection.rollback() // Rollback the transaction if no user was found
-      return res.status(404).json({ message: 'User not found' })
+      if (userDeleteResult.affectedRows === 0) {
+        await connection.rollback() // Rollback the transaction if no user was found
+        return res.status(404).json({ message: 'User not found' })
+      }
+
+      await connection.commit() // Commit the transaction if everything is fine
+      res.json({ message: 'User deleted successfully' })
+    } catch (error) {
+      console.error('Error executing MySQL query:', error)
+      if (connection) {
+        await connection.rollback() // Rollback the transaction in case of any error
+      }
+      return res
+        .status(500)
+        .json({ message: 'Internal Server Error', error: error.message })
+    } finally {
+      if (connection) {
+        await connection.release() // Release the connection in the finally block to ensure it's always executed
+      }
     }
-
-    await connection.commit() // Commit the transaction if everything is fine
-    res.json({ message: 'User deleted successfully' })
-  } catch (error) {
-    console.error('Error executing MySQL query:', error)
-    if (connection) {
-      await connection.rollback() // Rollback the transaction in case of any error
-    }
-    return res
-      .status(500)
-      .json({ message: 'Internal Server Error', error: error.message })
-  } finally {
-    if (connection) {
-      await connection.release() // Release the connection in the finally block to ensure it's always executed
-    }
-  }
-})
+  },
+)
 
 module.exports = router
